2010650b9c02d8acf7af77daa4dc1206297fcda9
gutierrez64 <gutierrezmatheus210@gmail.com>
2023-08-04 01:15:37 -0300

    class 01

---
+++ class01/main.py
@@ -0,0 +1,34 @@
+# TAKE 01 ------------------------------------------------
+
+
+print("Hello World!")
+
+
+# TAKE 02 ------------------------------------------------
+
+
+name = "David"
+print("Hello " + name)
+
+
+# TAKE 03 ------------------------------------------------
+
+
+age = 23
+if age < 21:
+    print("Get out HERE!")
+elif age >= 27:
+    print("No")
+else:
+    print("Hello baby")
+
+
+# TAKE 04 ------------------------------------------------
+
+
+subscribers = 0
+while subscribers <= 1000:
+    print('No')
+    subscribers += 1
+
+print('Hello motherfucker😎')
